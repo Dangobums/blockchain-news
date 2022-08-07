14ed3b95d9a2378967622bba2b5a77f696b36307
Tran Hoang Bao Long <19020110@vnu.edu.vn>
2022-08-07 18:22:04 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './index.css'
+import antdv from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+createApp(App).use(antdv).mount('#app')
